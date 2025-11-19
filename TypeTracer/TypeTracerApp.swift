1fb462a9e4e714d01678aeb9de066b428b0e33a8
RogerPan1203 <panyf200713@outlook.com>
2025-11-18 23:14:38 -0800

    Initial Commit

---
+++ TypeTracer/TypeTracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeTracerApp.swift
+//  TypeTracer
+//
+//  Created by Roger on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TypeTracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TypeTracerTests/TypeTracerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TypeTracerTests.swift
+//  TypeTracerTests
+//
+//  Created by Roger on 11/18/25.
+//
+
+import Testing
+@testable import TypeTracer
+
+struct TypeTracerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
